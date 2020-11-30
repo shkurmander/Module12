73c4476a85db3ff85146faeeb691b0742b8eded5
Vladimir Berdnikov <gazetter@mail.ru>
2020-11-30 22:27:36 +0700

    Добавлено 12.1.16

---
+++ Ex12.1.16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex12._1._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter color: ");
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is default!");
+                    break;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
