41892e5d82efcf68a105efca409d2f38afcd1e32
Vladimir Berdnikov <gazetter@mail.ru>
2020-11-30 21:52:33 +0700

    12.1.15

--- Ex12.1.14/Program.cs
+++ Ex12.1.14/Program.cs
@@ -6,8 +6,34 @@ namespace Ex12._1._14
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter color: ");
             var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!"); 
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else 
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+
+            Console.ReadKey();
         }
+
 
         
     }
