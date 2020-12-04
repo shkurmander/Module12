47551daae170f7d0b377f59bf1fb33b32c830fe5
Vladimir Berdnikov <gazetter@mail.ru>
2020-12-04 17:17:29 +0700

    Add 12.3.12

---
+++ Ex12.3.12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex12._3._12
+{
+    /// <summary>
+    /// Нахождение суммы элементов массива
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 5, 9, 6, 11, 2, 14, 19, 9 };
+            int sum = 0;
+
+            foreach (var item in array)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine($"Сумма элементов массива: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
