00839a89c2260e3d30d696fb8b00f07334923462
Vladimir Berdnikov <gazetter@mail.ru>
2020-12-04 17:30:08 +0700

    Add 12.3.14

---
+++ Ex12.3.14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex12._3._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, -2, 9, 0, -11, 2, 14, 19, -9 };
+            int amount = 0;
+            Console.WriteLine("Массив:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in array)
+            {
+                if (item > 0) ++amount;
+            }
+
+            Console.WriteLine($"Количество положительных чисел в массиве: {amount}");
+        }
+    }
+}
